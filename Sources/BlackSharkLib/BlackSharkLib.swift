4151afcc25a355c62eed77933eeb2f640f133cf8
Krister Berntsen <krister@spillmaker.no>
2025-06-07 17:29:22 +0200

    Added message-parser
    Still WIP.

--- Sources/BlackSharkLib/BlackSharkLib.swift
+++ Sources/BlackSharkLib/BlackSharkLib.swift
@@ -6,16 +6,64 @@ public class BlackSharkLib {
     private static let manufacturerDataIdentifier: Data = Data([0x8F, 0x03])
     
    
-    //region: Read actions
+    //
+    // Read
+    //
     private static let readCharacteristicsUUID   = Data([0xA0, 0x02])
+    
+    public protocol Message {
+        var rawData: Data { get }
+    }
+    
+    public struct CoolingState: Message {
+        public let rawData: Data
+        public let temperature: Int
+    }
+    
+    public struct FanState: Message {
+        public let rawData: Data
+        public let speed: Int // 0-100%
+    }
+    
+    public struct UnknownMessage: Message {
+        public let rawData: Data
+    }
     
     public static func getReadCharacteristicsUUID() -> Data {
         // Only one known characteristic for now
         return BlackSharkLib.readCharacteristicsUUID
     }
     
+    public static func parseMessages(_ data: Data) -> Message {
+        // data[0] is a split. First part of hex is always 8, second half is the lengthbit.
+        // data[1-2] is the command (that it responds to)
+        // data[3] is a spacer(?) seems to always be 0x00
+        
+        // 02 10 - Fan status
+        if data[1] == 0x02 && data[2] == 0x10 {
+            // Fan speed is either data[4] or data[5]
+            // Both are identical. I suspect one of them are the cooling value, but i dont know how to adjust them independently yet.
+            let speed = max(0, min(100, 100 - Int(data[4])))
+
+            return FanState(rawData: data, speed: speed)
+        }
+        
+        // 06 00 - Cooling status
+        if data[1] == 0x06 && data[2] == 0x00 {
+            // data[4] == Temperature
+            let temperature = Int(Int8(bitPattern: UInt8(data[4])))
+            
+            return CoolingState(rawData: data, temperature: temperature)
+        }
+        
+        // Return generig message
+        return UnknownMessage(rawData: data)
+    }
     
-    // Write actions
+    
+    //
+    // Write
+    //
     private static let writeCharacteristicsUUID  = Data([0xA0, 0x01])
     
     public static func getWriteCharacteristicsUUID() -> Data {
